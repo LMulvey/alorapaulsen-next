f146f91d1132d813db8a686d84e1b50f27e0bbcc
Lee Mulvey <lmulvey@me.com>
2024-10-06 11:07:54 -0600

    feat: updated logo + metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="p-4 flex flex-col items-center">
+      <div className="p-4 flex flex-col gap-8">
         <div className="max-w-[644px] max-h-[422px]">
           <Image
             alt="Alora Paulsen: Internet Researcher"
-            height={422}
+            height={271}
             sizes="100%"
             src="/logos/logo-stacked.svg"
-            width={644}
+            width={512}
           />
         </div>
         <h1 className={clsx('text-6xl', newsreader.className)}>
